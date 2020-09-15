c0f673c0eac5bc6b6519441a6bb20e4004fb749e
Nikita Ezzhev <nikita.ezzhev@akvelon.com>
2020-09-15 17:32:12 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ signal_caught() {
     touch /tmp/signal-catched-at-`date +%H:%M:%S`
     exit 1
 }
-trap signal_caught SIGHUP SIGINT SIGQUIT SIGTERM SIGKILL
+trap signal_caught SIGHUP SIGINT SIGQUIT SIGTERM SIGKILL EXIT
 while true; do sleep 1; echo .; done
