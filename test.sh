8eea489303d64036a22086f86d966bcf6b72f554
Nikita Ezzhev <nikita.ezzhev@akvelon.com>
2020-09-16 16:52:50 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-ps auxf
 signal_caught() {
-    echo "Script canceled...."
-    exit 1
-}
+  echo "Script cancellation...."
+  echo "Script cancellation succeeded...."
+  exit 0
+ }
 
-trap signal_caught SIGINT SIGTERM EXIT
+trap signal_caught SIGINT SIGTERM
 while true; do sleep 1; echo .; done
-
-$SHELL
